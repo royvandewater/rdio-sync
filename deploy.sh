787cad43076b460129b052e48038645c5f0f1bb4
Roy van de Water <dev+roy@integrumtech.com>
2014-07-14 19:13:02 -0700

    Pull deploy in from angular

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 start=`date +%s`
 
 HOST="deploy@rdio-sync.com"
+REPO="git@github.com:royvandewater/rdio-sync"
 APP_DIR=/home/deploy/apps/rdio-sync
 LOG_DIR=$APP_DIR/log
 CURRENT_DIR=$APP_DIR/current
 DESTINATION_DIR=$APP_DIR/releases/`date +%Y-%m-%d-%H-%M-%S`
+NPM_BIN="node_modules/.bin"
 
 function locally_do(){
   COMMAND=$@
@@ -32,23 +34,28 @@ function rsync_project(){
   fi
 }
 
-echo "compiling locally"
-locally_do "cake build"
 echo "creating directories"
 over_ssh_do "mkdir -p $APP_DIR/releases $APP_DIR/log $APP_DIR/forever"
-echo "starting rsync"
-rsync_project
+
+echo "cloning git"
+over_ssh_do "git clone --depth=1 $REPO $DESTINATION_DIR"
+
 echo "npm install"
 over_ssh_do "cd $DESTINATION_DIR && npm install --production"
+
+echo "compiling remotely"
+over_ssh_do "cd $DESTINATION_DIR && $NPM_BIN/cake build"
+
 echo "gzipping assets"
 over_ssh_do "cd $DESTINATION_DIR/public && \
-  for f in \$(find .); do \
+  for f in \$(find . -type f); do \
     gzip -9 -c \$f > \$f.gz; \
   done"
+
 echo "linking current"
 over_ssh_do "ln -nsf $DESTINATION_DIR $CURRENT_DIR"
-echo "Restarting Service"
 
+echo "Restarting Service"
 over_ssh_do "forever restart \
   -l $LOG_DIR/forever.log \
   -o $LOG_DIR/rdio-sync.log \
@@ -56,7 +63,6 @@ over_ssh_do "forever restart \
   --append \
   -p $APP_DIR/forever \
   -c coffee $CURRENT_DIR/src/application.coffee"
-
 
 end=`date +%s`
 runtime=$((end-start))
