90990304b7a437f5bcd7eccfd6b79df4100afaa3
Roy van de Water <sqrt+roy@octoblu.com>
2015-04-04 16:13:28 -0700

    Have only one docker run script

--- docker_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run \
-  --link rdio-sync-mysql:mysql \
-  -e RDIO_SECRET=$RDIO_SECRET \
-  -e RDIO_KEY=$RDIO_KEY \
-  -p 3004:3004 \
-  royvandewater/rdio-sync-api:devel
--- run-api-docker.sh
+++ run-api-docker.sh
@@ -1,6 +1,8 @@
-echo docker run \
-  -e MYSQL_CONNECT_STRING=$DOCKER_RDIO_MYSQL_CONNECT_STRING \
-  -e RDIO_KEY=$RDIO_KEY \
+#!/bin/bash
+
+docker run \
+  --link rdio-sync-mysql:mysql \
   -e RDIO_SECRET=$RDIO_SECRET \
-  --link trusting_kirch:mysql \
-  royvandewater/rdio-sync-api
+  -e RDIO_KEY=$RDIO_KEY \
+  -p 3004:3004 \
+  royvandewater/rdio-sync-api:devel
